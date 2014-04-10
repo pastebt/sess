834dbafd5ae1764f18b395ba4a9769bb9e4d77ee
pastebt <bt@hotmail.com>
2014-04-10 09:41:53 -0700

    move RequestWriter out from sessionInfo 修改：      sess.go

--- sess.go
+++ sess.go
@@ -13,20 +13,24 @@ const (
     COOKIENAME string = "MYGOSESSIONID"
 )
 
-type Session struct {
+
+type sessInfo struct {
     id string
     m *sync.Mutex
     expire time.Time
-    w *http.ResponseWriter
     data map[string]string
 }
 
+type Session struct {
+    si *sessInfo
+    w *http.ResponseWriter
+}
 
 var sessPool = struct {
     m *sync.Mutex
     path string
-    sess map[string]*Session
-} {&(sync.Mutex{}), "", make(map[string]*Session)}
+    sess map[string]*sessInfo
+} {&(sync.Mutex{}), "", make(map[string]*sessInfo)}
 
 
 // save file name to be used save session data
@@ -43,43 +47,46 @@ var logging = gslog.GetLogger("")
 func Start(w http.ResponseWriter, r *http.Request) (ses *Session) {
     c, e := r.Cookie(COOKIENAME)
     logging.Debug("e =", e)
+    var si *sessInfo
     if e == nil {
         logging.Debugf("n = %s, v = %s", c.Name, c.Value)
         sessPool.m.Lock()
         defer sessPool.m.Unlock()
-        ses = sessPool.sess[c.Value]
+        si = sessPool.sess[c.Value]
     }
-    logging.Debug("ses =", ses)
+    logging.Debug("si =", si)
     if ses == nil {
         id := "1234"    // TODO, generate id
-        ses = &Session{id:id, m:&(sync.Mutex{}),
+        si = &sessInfo{id:id, m:&(sync.Mutex{}),
                        data:make(map[string]string)}
     }
-    ses.w = &w
+    ses = &Session{si, &w}
     return
 }
 
 
 func (s *Session)Set(name string, value string) {
-    c := http.Cookie{Name:COOKIENAME, Value:s.id, Domain:"/"}
+    si := s.si
+    c := http.Cookie{Name:COOKIENAME, Value:si.id, Domain:"/"}
     http.SetCookie(*(s.w), &c)
-    s.m.Lock()
-    defer s.m.Unlock()
-    s.data[name] = value
+    si.m.Lock()
+    si.data[name] = value
+    si.m.Unlock()
+
     l := gslog.GetLogger("")
-    l.Debugf("n=%s, v=%s, s.id=%s", name, value, s.id)
+    l.Debugf("n=%s, v=%s, s.id=%s", name, value, si.id)
     // TODO update expire
     sessPool.m.Lock()
     defer sessPool.m.Unlock()
-    sessPool.sess[s.id] = s
+    sessPool.sess[si.id] = si
     l.Debug("sessPool =", sessPool.sess)
-    l.Debug("s =", s.data)
+    l.Debug("si =", si.data)
 }
 
 
 func (s *Session)Get(name string) (value string) {
-    s.m.Lock()
-    defer s.m.Unlock()
-    value = s.data[name]
+    s.si.m.Lock()
+    defer s.si.m.Unlock()
+    value = s.si.data[name]
     return
 }
