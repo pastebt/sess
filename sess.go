2808c36d30456c1cba72d7c4b40d085b8799820a
pastebt <bt@hotmail.com>
2014-04-10 22:19:37 -0700

    修改：      sess.go

--- sess.go
+++ sess.go
@@ -2,9 +2,14 @@ package sess
 
 
 import (
+    "os"
     "sync"
     "time"
+    "bytes"
     "net/http"
+    "io/ioutil"
+    "path/filepath"
+    "encoding/json"
     "github.com/pastebt/gslog"
 )
 
@@ -21,10 +26,12 @@ type sessInfo struct {
     data map[string]string
 }
 
+
 type Session struct {
     si *sessInfo
     w *http.ResponseWriter
 }
+
 
 var sessPool = struct {
     m *sync.Mutex
@@ -36,8 +43,42 @@ var sessPool = struct {
 // save file name to be used save session data
 // start the monitor thread to save it periodly
 // Should be called before server start
-func Init(sfn string) (err error) {
-    return
+func Init(sfn string) error {
+    sessPool.m.Lock()
+    defer sessPool.m.Unlock()
+
+    if sessPool.path == "" { // load from disk
+        logging.Debug("load session from disk")
+        sessPool.path = sfn
+        fs, e := filepath.Glob(filepath.Join(sfn, "*.sess"))
+        if e != nil { return e }
+        for _, f := range fs {
+            logging.Debug("load session", f)
+            dat, e := ioutil.ReadFile(f)
+            if e != nil { logging.Error(e); return e }
+            lines := bytes.SplitN(dat, []byte("\n"), 3)
+            si := &sessInfo{id:string(lines[0]), m:&sync.Mutex{}}
+            //si.expire = TODO
+            e = json.Unmarshal(lines[2], &si.data)
+            if e != nil { logging.Error(e); return e }
+            sessPool.sess[si.id] = si
+        }
+    } else {                // save to disk
+        logging.Debug("save session to disk")
+        for n, si := range sessPool.sess {
+            fout, e := os.OpenFile(filepath.Join(sfn, n) + ".sess",
+                                   os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0600)
+            if e != nil { logging.Error(e); return e }
+            _, e = fout.WriteString(si.id + "\n" + "1" + "\n")
+            if e != nil { logging.Error(e); return e }
+            j, e := json.Marshal(si.data)
+            if e != nil { logging.Error(e); return e }
+            fout.Write(j)
+            fout.Close()
+        }
+    }
+    _ = time.AfterFunc(time.Minute, func(){_ = Init(sfn)})
+    return nil
 }
 
 
@@ -49,13 +90,14 @@ func Start(w http.ResponseWriter, r *http.Request) (ses *Session) {
     logging.Debug("e =", e)
     var si *sessInfo
     if e == nil {
-        logging.Debugf("n = %s, v = %s", c.Name, c.Value)
+        logging.Debugf("Start n = %s, v = %s", c.Name, c.Value)
         sessPool.m.Lock()
-        defer sessPool.m.Unlock()
         si = sessPool.sess[c.Value]
+        sessPool.m.Unlock()
     }
-    logging.Debug("si =", si)
-    if ses == nil {
+    logging.Debug("Start si =", si)
+    if si == nil {
+        logging.Debug("Start new si")
         id := "1234"    // TODO, generate id
         si = &sessInfo{id:id, m:&(sync.Mutex{}),
                        data:make(map[string]string)}
@@ -67,7 +109,7 @@ func Start(w http.ResponseWriter, r *http.Request) (ses *Session) {
 
 func (s *Session)Set(name string, value string) {
     si := s.si
-    c := http.Cookie{Name:COOKIENAME, Value:si.id, Domain:"/"}
+    c := http.Cookie{Name:COOKIENAME, Value:si.id} //, Domain:"/"}
     http.SetCookie(*(s.w), &c)
     si.m.Lock()
     si.data[name] = value
@@ -87,6 +129,9 @@ func (s *Session)Set(name string, value string) {
 func (s *Session)Get(name string) (value string) {
     s.si.m.Lock()
     defer s.si.m.Unlock()
+    l := gslog.GetLogger("")
+    l.Debugf("Get s.id=%s", s.si.id)
+    l.Debug("Get si =", s.si.data)
     value = s.si.data[name]
     return
 }
